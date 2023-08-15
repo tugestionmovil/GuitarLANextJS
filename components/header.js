49ca5fec6a7318d152b1c8c1ef3b87117331f398
tugestionmovil <105019561+tugestionmovil@users.noreply.github.com>
2023-08-15 07:32:48 -0400

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Image from 'next/image'
-import Link from 'next/Link' 
+import Link from 'next/link' 
 import styles from '../styles/header.module.css'
 import {useRouter} from 'next/router'
 
