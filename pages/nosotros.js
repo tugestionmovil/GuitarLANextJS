c4cba1862cfe3d3687d3012f9f9746f324d791fe
tugestionmovil <105019561+tugestionmovil@users.noreply.github.com>
2023-08-15 07:34:57 -0400

    Update nosotros.js

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,4 +1,4 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 import Layout from '../components/layout'
 import Image from 'next/image'
 import styles from '../styles/nosotros.module.css'
